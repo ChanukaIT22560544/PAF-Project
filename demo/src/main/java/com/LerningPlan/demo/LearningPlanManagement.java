a9b2dbc6a510d4898ab832c04b87868e774d4332
Chanuka Lakshan Koralage <131335900+ChanukaIT22560544@users.noreply.github.com>
2025-04-23 01:48:01 +0530

    update main file

--- demo/src/main/java/com/LerningPlan/demo/LearningPlanManagement.java
+++ demo/src/main/java/com/LerningPlan/demo/LearningPlanManagement.java
@@ -1,13 +1,12 @@
+
 package com.LerningPlan.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class LearningPlanManagement {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(LearningPlanManagement.class, args);
 	}
-
 }
